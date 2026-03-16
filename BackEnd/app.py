6ce962767527eb142a800e84d10746e0ab856252
Leiva230-2 <peteraviel1@gmail.com>
2026-03-16 16:01:58 +0700

    chore: add mock 3-second delay to API

--- BackEnd/app.py
+++ BackEnd/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request
 from flask_cors import CORS
-
+import time
 
 app = Flask(__name__)
 CORS(app)
@@ -9,6 +9,7 @@ CORS(app)
 
 @app.route('/api/scan', methods=['POST'])
 def scan_image():
+    time.sleep(3)
     return {"status": "success", "message": "Door is open!"}
 
 
