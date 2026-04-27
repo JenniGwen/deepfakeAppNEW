005e1286acc9f18ee10531caf2dda45a48c00d1c
JenniGwen <jennigwen25@gmail.com>
2026-04-27 12:34:18 +0700

    Update : Git Ignore .env

--- BackEnd/run.py
+++ BackEnd/run.py
@@ -1,6 +1,6 @@
 # File: BackEnd/run.py
 
-from flask import Flask
+from flask import Flask, request, Response
 from flask_cors import CORS
 
 from controllers.analysis_controllers import scan_image
@@ -12,6 +12,23 @@ from middlewares.auth import token_required
 
 app = Flask(__name__)
 CORS(app, resources={r"/api/*": {"origins": "*"}}, supports_credentials=False)
+
+@app.after_request
+def add_cors_headers(response):
+    response.headers["Access-Control-Allow-Origin"] = "*"
+    response.headers["Access-Control-Allow-Methods"] = "GET, POST, PUT, DELETE, OPTIONS"
+    response.headers["Access-Control-Allow-Headers"] = "Content-Type, Authorization"
+    return response
+
+@app.before_request
+def handle_preflight():
+    if request.method == "OPTIONS":
+        res = Response()
+        res.headers["Access-Control-Allow-Origin"] = "*"
+        res.headers["Access-Control-Allow-Methods"] = "GET, POST, PUT, DELETE, OPTIONS"
+        res.headers["Access-Control-Allow-Headers"] = "Content-Type, Authorization"
+        res.headers["Access-Control-Max-Age"] = "86400"
+        return res, 200
 
 # --- PUBLIC ROUTES ---
 app.add_url_rule('/api/test-db', view_func=test_connection, methods=['GET'])
